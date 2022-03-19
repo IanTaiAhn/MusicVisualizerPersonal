65d6e71cc03b30d45d6d028f6fd3af5c4bb8ac67
iantaiahn <iantaiahn@DESKTOP-7NC019E.lan>
2022-03-18 18:45:46 -0600

    Deleted the two print statements created on the remote repository

--- MusicVisualizer/src/CommitWhileConnected/Test.java
+++ MusicVisualizer/src/CommitWhileConnected/Test.java
@@ -4,8 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("editing from the remote repo");
-		System.out.println("woohoo! I think I am successfully pulling from a repo");
+		
 	}
 
 }
